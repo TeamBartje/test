f77ce594dd59c54a42fb5fc1b355b209b4e9c122
Matthias Alleman <matthias.alleman@hotmail.com>
2017-11-07 10:26:39 +0100

    arduino toegevoegd

---
+++ Drukknop.ino
@@ -0,0 +1,14 @@
+#define BUTTON 7
+#define LED 13
+void setup() {
+  pinMode(LED,OUTPUT);
+  pinMode(BUTTON, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+ if (digitalRead(BUTTON) == HIGH){
+  digitalWrite (LED, LOW);
+ }
+ else digitalWrite(LED, HIGH);
+}
